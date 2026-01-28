17d8076e9bf68deba430c924449f19ab499f2fec
SanketNyaynite <sanket.nyaynite@gmail.com>
2026-01-27 18:31:53 -0600

    Add a custom parameter constraint.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRouting(options =>
+{
+    options.ConstraintMap.Add("pos", typeof(PositionConstraint));
+});
+
 var app = builder.Build();
 
 //app.MapGet("/employees", async (HttpContext context) =>
@@ -40,15 +46,37 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"Deleted an Employee: {context.Request.RouteValues["id"]}");
     });
 
-    endpoints.MapGet("/{category=shirts}/{size=medium}/{id=0}", async (HttpContext context) =>
+    //endpoints.MapGet("/{category=shirts}/{size=medium}/{id?}", async (HttpContext context) =>
+    //{
+    //    await context.Response.WriteAsync($"Get category: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]} and ID is: {context.Request.RouteValues["id"]}");
+    //});
+
+    endpoints.MapGet("/employees/{id:int}", async (HttpContext context) =>
     {
-        await context.Response.WriteAsync($"Get category: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]} and ID is: {context.Request.RouteValues["id"]}");
+        await context.Response.WriteAsync($"Get employee: {context.Request.RouteValues["id"]}");
+    });
+
+    endpoints.MapGet("/employees/positions/{position:pos}", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync($"Get employees under position: {context.Request.RouteValues["position"]}");
     });
 });
 
 app.Run();
 
-
+class PositionConstraint : IRouteConstraint
+{
+    public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+    {
+        if (!values.ContainsKey(routeKey)) return false;
+        if (values[routeKey] is null) return false;
+        
+        if (values[routeKey].ToString().Equals("manager", StringComparison.OrdinalIgnoreCase) ||
+            values[routeKey].ToString().Equals("developer", StringComparison.OrdinalIgnoreCase))        
+            return true;
+        return false;
+    }
+}
 
 
 
